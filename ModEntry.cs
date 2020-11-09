f5306b866bfdb275fe777efb825649f12ab276aa
Enaium <32991121+Enaium@users.noreply.github.com>
2020-11-09 16:19:31 +0800

    init

---
+++ ModEntry.cs
@@ -0,0 +1,12 @@
+﻿using StardewModdingAPI;
+
+namespace EnaiumToolKit
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            
+        }
+    }
+}
