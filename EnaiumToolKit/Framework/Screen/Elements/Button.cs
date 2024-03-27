b9d298de13d6b700ec753e81b1a2730da2b55c6a
Enaium <wyy-666@hotmail.com>
2024-03-27 12:02:55 +0800

    update to dotnet6

---
+++ EnaiumToolKit/Framework/Screen/Elements/Button.cs
@@ -0,0 +1,27 @@
+﻿using EnaiumToolKit.Framework.Utils;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace EnaiumToolKit.Framework.Screen.Elements;
+
+public class Button : Element
+{
+    public Button(string title, string description) : base(title, description)
+    {
+    }
+
+    public override void Render(SpriteBatch b, int x, int y)
+    {
+        Hovered = Render2DUtils.IsHovered(Game1.getMouseX(), Game1.getMouseY(), x, y, Width, Height);
+        Render2DUtils.DrawButton(b, x, y, Width, Height, Color.Wheat);
+        if (Hovered)
+        {
+            IClickableMenu.drawTextureBox(b, Game1.mouseCursors, new Rectangle(375, 357, 3, 3), x, y, Width, Height,
+                Color.Black, 4f, false);
+        }
+
+        FontUtils.DrawHvCentered(b, Title, x + Width / 2, y + Height / 2);
+    }
+}
---
+++ EnaiumToolKit/Framework/Utils/FontUtils.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+using StardewValley.BellsAndWhistles;
+
+namespace EnaiumToolKit.Framework.Utils;
+
+public class FontUtils
+{
+    public static void Draw(SpriteBatch b, string text, int x, int y)
+    {
+        Utility.drawTextWithShadow(b, text, Game1.dialogueFont,
+            new Vector2(x, y), Game1.textColor, 1f,
+            -1f,
+            -1, -1, 0.0f);
+    }
+
+    public static void DrawHCentered(SpriteBatch b, string text, int x, int y)
+    {
+        var v = Game1.dialogueFont.MeasureString(text);
+        Utility.drawTextWithShadow(b, text, Game1.dialogueFont,
+            new Vector2(x - GetWidth(text) / 2f, y) + v, Game1.textColor, 1f,
+            -1f,
+            -1, -1, 0.0f);
+    }
+
+    public static void DrawVCentered(SpriteBatch b, string text, int x, int y)
+    {
+        Utility.drawTextWithShadow(b, text, Game1.dialogueFont,
+            new Vector2(x, y - GetHeight(text) / 2f), Game1.textColor, 1f,
+            -1f,
+            -1, -1, 0.0f);
+    }
+
+    public static void DrawHvCentered(SpriteBatch b, string text, int x, int y)
+    {
+        Utility.drawTextWithShadow(b, text, Game1.dialogueFont,
+            new Vector2(x - GetWidth(text) / 2f, y - GetHeight(text) / 2f),
+            Game1.textColor, 1f,
+            -1f,
+            -1, -1, 0.0f);
+    }
+
+
+    public static int GetWidth(string text)
+    {
+        return SpriteText.getWidthOfString(text);
+    }
+
+    public static int GetHeight(string text)
+    {
+        return SpriteText.getHeightOfString(text);
+    }
+}
