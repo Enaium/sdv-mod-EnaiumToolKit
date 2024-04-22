21627eca997b95297b37de59f7f58f1f4dbd5275
Enaium <wyy-666@hotmail.com>
2024-04-22 17:35:03 +0800

    fix block pos

--- EnaiumToolKit/Framework/Screen/Elements/SliderBar.cs
+++ EnaiumToolKit/Framework/Screen/Elements/SliderBar.cs
@@ -12,7 +12,6 @@ public class SliderBar : BaseButton
     private int _min;
     private int _max;
 
-    private int _sliderOffset;
     public bool Dragging;
 
     public SliderBar(string title, string description, int min, int max) : base(title, description)
@@ -23,12 +22,14 @@ public class SliderBar : BaseButton
 
     public override void Render(SpriteBatch b, int x, int y)
     {
+        var blockSize = 20;
+
         if (Hovered)
         {
             if (Dragging)
             {
-                _sliderOffset = MathHelper.Clamp(Game1.getMouseX() - x, 0, Width - 20);
-                Current = (int)(_min + MathHelper.Clamp((Game1.getMouseX() - x) / (float)Width, 0, 1) * (_max - _min));
+                Current = (int)(_min + MathHelper.Clamp((Game1.getMouseX() - x) / (float)(Width - blockSize), 0, 1) *
+                    (_max - _min));
             }
         }
         else
@@ -36,13 +37,8 @@ public class SliderBar : BaseButton
             Dragging = false;
         }
 
-        Render2DUtils.DrawButton(b,
-            x + (_sliderOffset != 0 ? _sliderOffset :
-                Current != 0 ? (int)MathHelper.Clamp((Current - _min) / (float)(_max - _min), 0, 1) * Width - 20 :
-                _sliderOffset),
-            y,
-            20, Height,
-            Color.Wheat);
+        var sliderOffset = (Width - blockSize) * (Current - _min) / (_max - _min);
+        Render2DUtils.DrawButton(b, x + sliderOffset, y, blockSize, Height, Color.Wheat);
 
         FontUtils.DrawHvCentered(b, $"{Title}:{Current}", x, y, Width, Height);
         base.Render(b, x, y);
